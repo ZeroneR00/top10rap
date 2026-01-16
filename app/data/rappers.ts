c981cb16ea62d394bad3d0f98a02bf3fed7ce0ff
ZeroneR00 <andrey.malyga2024@gmail.com>
2026-01-16 13:39:07 +0300

    start

---
+++ app/data/rappers.ts
@@ -0,0 +1,20 @@
+import { Rapper } from "../types"
+
+export const rappers: Rapper[] = [{
+    id: "1",
+    slug: "kanye-west",
+    name: "Kanye West",
+    realName: "Kanye Omari West",
+    image: "https://via.placeholder.com/150",
+    rank: 1,
+    bio: "Kanye West is an American rapper, songwriter, and fashion designer. He is one of the most influential and successful artists of the 21st century."
+},
+{
+    id: "2",
+    slug: "kizaru",
+    name: "Kizaru",
+    image: "https://via.placeholder.com/150",
+    rank: 2,
+    bio: "Kizaru is a Russian rapper. He is one of the most influential and successful artists of the 21st century."
+}
+]
---
+++ app/types/index.ts
@@ -0,0 +1,10 @@
+export interface Rapper {
+    id: string;
+    slug: string;
+    name: string;
+    realName?: string;
+    image: string;
+    rank: number;
+    bio?: string;
+    // more if need
+  }
